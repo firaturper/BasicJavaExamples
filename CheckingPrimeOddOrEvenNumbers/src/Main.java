cf838bfbc6d204d08d9d522d3ce432fa8e7bca7a
firaturper <firaturper88@gmail.com>
2021-10-13 20:53:13 +0300

    Add files via upload

---
+++ CheckingPrimeOddOrEvenNumbers/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please Add a Number: ");
+        Boolean primeNum = true;
+        int num = scan.nextInt();
+
+        if(num%2==0) {System.out.print("Number is Even and ");}
+        else {System.out.print("Number is Odd and ");}
+        for(int i=2; i<=num/2; i++) {
+            if(num%i==0) {
+                primeNum = false;
+                break;
+            }
+        }
+
+        if(!primeNum) {System.out.print("not Prime");}
+        else {System.out.print("Prime");}
+    }
+}
