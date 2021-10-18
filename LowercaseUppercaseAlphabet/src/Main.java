c9a4313593401efd1388da1de24fc905ef70708c
firaturper <firaturper88@gmail.com>
2021-10-18 14:26:14 +0300

    Add files via upload

---
+++ LowercaseUppercaseAlphabet/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        for(char lower='a';lower<='z';++lower) {
+            //lowercase letters are selected from the ASCII code
+            System.out.print(lower+"-");
+        }
+        System.out.println();
+        System.out.println("****************************************************");
+        for (char upper='A';upper<='Z';++upper) {
+            //uppercase letters are selected from the ASCII code
+            System.out.print(upper+"-");
+        }
+    }
+}
